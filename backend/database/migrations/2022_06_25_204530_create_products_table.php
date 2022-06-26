55a1eda55df39fccb6c71e6e9c28d138fdbf8dc0
fariz <sangater123@gmail.com>
2022-06-26 13:24:35 +0700

    reafctor Id Product

--- backend/database/migrations/2022_06_25_204530_create_products_table.php
+++ backend/database/migrations/2022_06_25_204530_create_products_table.php
@@ -9,7 +9,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->bigIncrements('id');
             $table->string('name', 100);
             $table->integer('price');
             $table->timestamps();
